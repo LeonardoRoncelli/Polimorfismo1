82cd54ac2f407254e1aa77453c5596d926aa5dcc
RONCELLI.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-11-15 09:10:59 +0100

    Attributi classe Spettacolo

--- src/Spettacolo.java
+++ src/Spettacolo.java
@@ -1,3 +1,8 @@
 public class Spettacolo {
-
+    private String titolo;
+    private String nomeCompagnia;
+    private int data;
+    private String orario;
+    private static final double costo=15;
+    private static final int postiDisponibili=100;
 }
